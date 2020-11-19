d53b5b85b19fa039f88394c7bf1833feda760887
Marylise van Geel <vangeel.m@outlook.com>
2020-11-19 15:37:12 +0100

    Question 11

--- test/ProfCoursTest.php
+++ test/ProfCoursTest.php
@@ -310,15 +310,14 @@ class ProfCoursTest extends TestCase
         $this->assertTrue($val, "Update du prof num $idProf ...\n");
 
         // Cours
-        
-        /**
-        *
-        * Question 11 :	Dans la fonction « testUpdateOne() », 
-        * s’inspirer de test de la modification du prof avec idProf= 10 pour tester la modification du cours dans ayant comme idCours = 9.
-        *
-        */
-       
-        
+        $cours = new Cours($this->intitule, $this->duree);
+        $val = $cours->updateOne($conn, $idCours);
+        $expected_cours_str = $cours->__toString();
+        $record_cours = Cours::printOne($conn, $idCours);
+        $this->assertEquals($expected_cours_str, $record_cours->__toString(), "Update du cours $idCours ...\n");
+        $this->assertTrue($val, "Update du cours num $idCours ...\n");
+
+
         
         // Prof
         
